828ba1c7da421c5336ab3ea043348c8538cd343f
Luke Dashjr <luke-jr+git@utopios.org>
2016-02-15 06:38:09 +0000

    Qt/Options: Configure mempoolreplacement using rwconf
    (cherry picked from commit 5a661b857fa6368a792dd8c05214fc86eafd23c3)

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -81,6 +81,7 @@ public:
         maxuploadtarget,
         peerbloomfilters,       // bool
         peerblockfilters,       // bool
+        mempoolreplacement,
         OptionIDRowCount,
     };
 
