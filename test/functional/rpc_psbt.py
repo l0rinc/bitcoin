572e792b918d3652e4511f3292c65164612e0097
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-12-04 16:12:07 +0000

    qa: Test fundrawtransaction and walletcreatefundedpsbt with min_conf
    Github-Pull: #14641
    Rebased-From: 4138cb304b84341c0284617d8b70e6ed880d8763
    (cherry picked from commit af7c9d152aa27e0925f592cf823052a9e4538946)

--- test/functional/rpc_psbt.py
+++ test/functional/rpc_psbt.py
@@ -534,7 +534,8 @@ class PSBTTest(BitcoinTestFramework):
 
     def run_test(self):
         # Create and fund a raw tx for sending 10 BTC
-        psbtx1 = self.nodes[0].walletcreatefundedpsbt([], {self.nodes[2].getnewaddress():10})['psbt']
+        assert_raises_rpc_error(-4, "Insufficient funds", self.nodes[0].walletcreatefundedpsbt, inputs=[], outputs={self.nodes[2].getnewaddress():1}, options={'min_conf': 201})
+        psbtx1 = self.nodes[0].walletcreatefundedpsbt(inputs=[], outputs={self.nodes[2].getnewaddress():11}, options={'min_conf': 200})['psbt']
 
         self.log.info("Test for invalid maximum transaction weights")
         dest_arg = [{self.nodes[0].getnewaddress(): 1}]
