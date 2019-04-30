35f02ee359fb23b852a2ce6cf578ea25f7acfd2e
Luke Dashjr <luke-jr+git@utopios.org>
2019-04-30 23:31:17 +0000

    GUI: Add Pairing tab to disablewallet mode
    (cherry picked from commit e7eb0f26c7d677e3e0428221ac14e2131efe066b)

--- src/qt/rpcconsole.h
+++ src/qt/rpcconsole.h
@@ -22,6 +22,7 @@
 #include <QWidget>
 
 class GuiNetWatch;
+class PairingPage;
 class PlatformStyle;
 class RPCExecutor;
 class WalletModel;
@@ -55,6 +56,7 @@ public:
     }
 
     void setClientModel(ClientModel *model = nullptr, int bestblock_height = 0, int64_t bestblock_date = 0, double verification_progress = 0.0);
+    void addPairingTab();
 
 #ifdef ENABLE_WALLET
     void addWallet(WalletModel* walletModel);
@@ -73,7 +75,8 @@ public:
         INFO,
         CONSOLE,
         GRAPH,
-        PEERS
+        PEERS,
+        PAIRING,
     };
 
     std::vector<TabTypes> tabs() const;
@@ -169,6 +172,7 @@ private:
     Ui::RPCConsole* const ui;
     ClientModel *clientModel = nullptr;
     std::map<TabTypes, QWidget*> m_tabs;
+    PairingPage *m_tab_pairing{nullptr};
     GuiNetWatch *netwatch = nullptr;
     QStringList history;
     int historyPtr = 0;
