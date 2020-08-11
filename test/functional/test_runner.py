3385f8b38b2331a65076e2a769bacc24d724f21a
Roman Zeyde <me@romanzey.de>
2020-08-11 17:57:17 +0300

    RPC: Add getblocklocations call
    This RPC allows the client to retrieve the file system locations
    of the confirmed blocks and their undo data, to allow building
    efficient indexes outside of Bitcoin Core.
    
    An example usage is described here:
    https://github.com/romanz/electrs/issues/308
    
    By using the new RPC, it is possible to build an address-based
    index taking ~24GB and a txindex taking ~6GB (as of Dec. 2020).
    
    (cherry picked from commit 620e79b72f6dd6eb0bd2798c4da893081d8e7f39)

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -316,6 +316,7 @@ BASE_SCRIPTS = [
     'wallet_resendwallettransactions.py',
     'wallet_fallbackfee.py',
     'rpc_dumptxoutset.py',
+    'rpc_getblocklocations.py',
     'feature_minchainwork.py',
     'rpc_estimatefee.py',
     'p2p_private_broadcast.py',
