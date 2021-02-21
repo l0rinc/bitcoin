014cc81c71b8f6075233f364f8393ffda8393f9d
Dan Benjamin <danben@gmail.com>
2021-02-21 18:01:31 -0500

    Added a field to the output of gettransaction/listtransactions to indicate whether the given transaction is in the mempool.
    Github-Pull: #21260
    Rebased-From: 46bf0b7b5d8c44bd7032c473f9878cfb59018161
    (cherry picked from commit 86f76777c6bc58d22b71802aca039415593920d1)

--- test/functional/wallet_basic.py
+++ test/functional/wallet_basic.py
@@ -719,6 +719,7 @@ class WalletTest(BitcoinTestFramework):
             'details',
             'fee',
             'hex',
+            'in_mempool',
             'lastprocessedblock',
             'mempoolconflicts',
             'time',
