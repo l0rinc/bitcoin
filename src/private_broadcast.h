ad3a380dfe9be96088fa63a98675eb23305891b4
Vasil Dimov <vd@FreeBSD.org>
2023-04-19 15:40:30 +0200

    net_processing: handle ConnectionType::PRIVATE_BROADCAST connections
    For connections of type `ConnectionType::PRIVATE_BROADCAST`:
    * After receiving VERACK, send a transaction from the list of
      transactions for private broadcast and disconnect
    * Don't process any messages after VERACK (modulo `GETDATA` and `PONG`)
    * Don't send any messages other than the minimum required for the
      transaction send - `INV`, `TX`, `PING`.

--- src/private_broadcast.h
+++ src/private_broadcast.h
@@ -7,6 +7,7 @@
 
 #include <net.h>
 #include <primitives/transaction.h>
+#include <primitives/transaction_identifier.h>
 #include <sync.h>
 #include <threadsafety.h>
 #include <util/time.h>
@@ -19,6 +20,10 @@
 /**
  * Store a list of transactions to be broadcast privately. Supports the following operations:
  * - Add a new transaction
+ * - Pick a transaction for sending to one recipient
+ * - Query which transaction has been picked for sending to a given recipient node
+ * - Mark that a confirmation of reception for a transaction by a given recipient node
+ * - Query whether a given recipient node has confirmed reception
  */
 class PrivateBroadcast
 {
@@ -31,12 +36,61 @@ public:
      */
     bool Add(const CTransactionRef& tx) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
 
+    /**
+     * Pick the transaction that has been sent fewest times and least recently.
+     * @param[in] will_send_to_nodeid Will remember that the returned transaction
+     * was picked for sending to this node.
+     * @return Most urgent transaction or nullopt if there are no transactions.
+     */
+    std::optional<CTransactionRef> PickTxForSend(const NodeId& will_send_to_nodeid)
+        EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
+
+    /**
+     * Get the transaction that was picked for sending to a given node by PickTxForSend().
+     * @param[in] nodeid Node to which a transaction is being (or was) sent.
+     * @return Transaction or nullopt if the nodeid is unknown.
+     */
+    std::optional<CTransactionRef> GetTxForNode(const NodeId& nodeid)
+        EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
+
+    /**
+     * Mark that the node has confirmed reception of the transaction we sent it by
+     * responding with `PONG` to our `PING` message.
+     * @param[in] nodeid Node that we sent a transaction to.
+     */
+    void NodeConfirmedReception(const NodeId& nodeid)
+        EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
+
+    /**
+     * Check if the node has confirmed reception of the transaction.
+     * @retval true Node has confirmed, `NodeConfirmedReception()` has been called.
+     * @retval false Node has not confirmed, `NodeConfirmedReception()` has not been called.
+     */
+    bool DidNodeConfirmReception(const NodeId& nodeid)
+        EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
+
 private:
     /// Status of a transaction sent to a given node.
     struct SendStatus {
         NodeId nodeid; /// Node to which the transaction will be sent (or was sent).
         NodeClock::time_point picked; ///< When was the transaction picked for sending to the node.
         std::optional<NodeClock::time_point> confirmed; ///< When was the transaction reception confirmed by the node (by PONG).
+    };
+
+    /// Cumulative stats from all the send attempts for a transaction. Used to prioritize transactions.
+    struct Priority {
+        size_t num_picked{0}; ///< Number of times the transaction was picked for sending.
+        NodeClock::time_point last_picked{}; ///< The most recent time when the transaction was picked for sending.
+        size_t num_confirmed{0}; ///< Number of nodes that have confirmed reception of a transaction (by PONG).
+        NodeClock::time_point last_confirmed{}; ///< The most recent time when the transaction was confirmed.
+
+        std::strong_ordering operator<=>(const Priority& other) const;
+    };
+
+    /// A pair of a transaction and a sent status for a given node. Convenience return type of GetSendStatusByNode().
+    struct TxAndSendStatusForNode {
+        CTransactionRef& tx;
+        SendStatus& send_status;
     };
 
     // No need for salted hasher because we are going to store just a bunch of locally originating transactions.
@@ -54,6 +108,20 @@ private:
             return a->GetHash() == b->GetHash() && a->GetWitnessHash() == b->GetWitnessHash();
         }
     };
+
+    /**
+     * Derive the sending priority of a transaction.
+     * @param[in] sent_to List of nodes that the transaction has been sent to.
+     */
+    static Priority DerivePriority(const std::vector<SendStatus>& sent_to);
+
+    /**
+     * Find which transaction we sent to a given node (marked by PickTxForSend()).
+     * @return That transaction together with the send status or nullopt if we did not
+     * send any transaction to the given node.
+     */
+    std::optional<TxAndSendStatusForNode> GetSendStatusByNode(const NodeId& nodeid)
+        EXCLUSIVE_LOCKS_REQUIRED(m_mutex);
 
     mutable Mutex m_mutex;
     std::unordered_map<CTransactionRef, std::vector<SendStatus>, CTransactionRefHash, CTransactionRefComp>
