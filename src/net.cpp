a0436e8c6cd9b75016c04edf89fff636b07c9bda
Matthew Zipkin <pinheadmz@gmail.com>
2023-07-28 14:47:27 -0400

    net: only allow 8 simultaneous forced inbound connections
    Github-Pull: #27600
    Rebased-From: 75868022a904c1f77871abf962bf9b88a9c5faf6
    (cherry picked from commit 2b3e19e9e854722b7bb04d11a579dc9df9435bda)

--- src/net.cpp
+++ src/net.cpp
@@ -1780,6 +1780,7 @@ void CConnman::CreateNodeFromAcceptedSocket(std::unique_ptr<Sock>&& sock,
     AssertLockNotHeld(m_nodes_mutex);
 
     int nInbound = 0;
+    int nForced{0};
 
     const bool inbound_onion = std::find(m_onion_binds.begin(), m_onion_binds.end(), addr_bind) != m_onion_binds.end();
 
@@ -1791,6 +1792,7 @@ void CConnman::CreateNodeFromAcceptedSocket(std::unique_ptr<Sock>&& sock,
         LOCK(m_nodes_mutex);
         for (const CNode* pnode : m_nodes) {
             if (pnode->IsInboundConn()) nInbound++;
+            if (pnode->m_forced_inbound) nForced++;
         }
     }
 
@@ -1828,8 +1830,15 @@ void CConnman::CreateNodeFromAcceptedSocket(std::unique_ptr<Sock>&& sock,
         return;
     }
 
+    bool forced{false};
     if (nInbound >= m_max_inbound)
     {
+        // Protect from force evicting everyone
+        if (nForced >= MAX_FORCED_INBOUND_CONNECTIONS) {
+            LogPrint(BCLog::NET, "connection from %s dropped (too many forced inbound)\n", addr.ToStringAddrPort());
+            return;
+        }
+
         // If the inbound connection attempt is granted ForceInbound permission, try a little harder
         // to make room by evicting a peer we may not have otherwise evicted.
         if (!AttemptToEvictConnection(NetPermissions::HasFlag(permission_flags, NetPermissionFlags::ForceInbound))) {
@@ -1837,6 +1846,9 @@ void CConnman::CreateNodeFromAcceptedSocket(std::unique_ptr<Sock>&& sock,
             LogDebug(BCLog::NET, "failed to find an eviction candidate - connection dropped (full)\n");
             return;
         }
+
+        // We kicked someone out
+        forced = true;
     }
 
     NodeId id = GetNewNodeId();
@@ -1865,6 +1877,7 @@ void CConnman::CreateNodeFromAcceptedSocket(std::unique_ptr<Sock>&& sock,
                              CNodeOptions{
                                  .permission_flags = permission_flags,
                                  .prefer_evict = discouraged,
+                                 .forced_inbound = forced,
                                  .recv_flood_size = nReceiveFloodSize,
                                  .use_v2transport = use_v2transport,
                              });
@@ -4075,6 +4088,7 @@ CNode::CNode(NodeId idIn,
       m_dest(addrNameIn),
       m_inbound_onion{inbound_onion},
       m_prefer_evict{node_opts.prefer_evict},
+      m_forced_inbound{node_opts.forced_inbound},
       nKeyedNetGroup{nKeyedNetGroupIn},
       m_network_key{network_key},
       m_conn_type{conn_type_in},
