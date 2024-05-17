fdfff4a0558bf184ac8c0badfcce4055d47bc3b2
Luke Dashjr <luke-jr+git@utopios.org>
2024-05-17 16:51:21 +0000

    RPC/net: disconnectnode: Support disconnecting by IP (without a port)
    (cherry picked from commit 7e3988fe54967a04bd3d18867c7d278f3b530d15)

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -474,15 +474,19 @@ static RPCMethod disconnectnode()
 
     if (address && !node_id) {
         /* handle disconnect-by-address */
-        if (address_arg.get_str().find('/') != std::string::npos) {
+        const bool only_subnet{address_arg.get_str().find('/') != std::string::npos};
+        if (only_subnet) {
+            success = false;
+        } else {
+            success = connman.DisconnectNode(address_arg.get_str());
+        }
+        if (!success) {
             const CSubNet subnet = LookupSubNet(address_arg.get_str());
             if (subnet.IsValid()) {
                 success = connman.DisconnectNode(subnet);
-            } else {
+            } else if (only_subnet) {
                 throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid subnet");
             }
-        } else {
-            success = connman.DisconnectNode(address_arg.get_str());
         }
     } else if (!id_arg.isNull() && (address_arg.isNull() || (address_arg.isStr() && address_arg.get_str().empty()))) {
         /* handle disconnect-by-id */
