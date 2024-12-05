5c13c899eb1e8f1334cdb54125e0a14acb6f0fcd
Luke Dashjr <luke-jr+git@utopios.org>
2024-12-05 22:05:14 +0000

    Bump default dbfilesize to 64 MiB
    Actual benchmarks suggest 64 MiB performs best
    
    (cherry picked from commit 5c4f46893da232d997468811b5539939d522deb1)

--- src/dbwrapper.h
+++ src/dbwrapper.h
@@ -32,7 +32,7 @@ class Env;
 
 static const size_t DBWRAPPER_PREALLOC_KEY_SIZE = 64;
 static const size_t DBWRAPPER_PREALLOC_VALUE_SIZE = 1024;
-static const size_t DBWRAPPER_MAX_FILE_SIZE{32_MiB};
+static const size_t DBWRAPPER_MAX_FILE_SIZE{64_MiB};
 
 //! User-controlled performance and debug options.
 struct DBOptions {
