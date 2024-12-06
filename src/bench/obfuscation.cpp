ce52e4cba673dedf19183fdbf06634d451eecd1b
Lőrinc <pap.lorinc@gmail.com>
2024-12-06 16:18:03 +0100

    bench: make ObfuscationBench more representative
    A previous PR already solved the tiny byte-array-xors during serialization, so it makes sense to keep focusing on performance of bigger continuous chunks.
    
    Since 31 byte xor-keys are not used in the codebase, using the common size (8 bytes) makes the benchmarks more realistic.
    
    This also renames the file from `xor` to `obfuscation` to enable scripted diff name unification later.
    
    > C++ compiler .......................... GNU 14.2.0
    
    |             ns/byte |              byte/s |    err% |        ins/byte |        cyc/byte |    IPC |       bra/byte |   miss% |     total | benchmark
    |--------------------:|--------------------:|--------:|----------------:|----------------:|-------:|---------------:|--------:|----------:|:----------
    |                0.84 |    1,184,138,235.64 |    0.0% |            9.01 |            3.03 |  2.971 |           1.00 |    0.1% |      5.50 | `ObfuscationBench`
    
    > C++ compiler .......................... Clang 20.1.7
    
    |             ns/byte |              byte/s |    err% |        ins/byte |        cyc/byte |    IPC |       bra/byte |   miss% |     total | benchmark
    |--------------------:|--------------------:|--------:|----------------:|----------------:|-------:|---------------:|--------:|----------:|:----------
    |                0.89 |    1,124,087,330.23 |    0.1% |            6.52 |            3.20 |  2.041 |           0.50 |    0.2% |      5.50 | `ObfuscationBench`

---
+++ src/bench/obfuscation.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://opensource.org/license/mit/.
+
+#include <bench/bench.h>
+#include <random.h>
+#include <span.h>
+#include <streams.h>
+#include <util/byte_units.h>
+
+#include <cstddef>
+#include <vector>
+
+static void ObfuscationBench(benchmark::Bench& bench)
+{
+    FastRandomContext rng{/*fDeterministic=*/true};
+    constexpr size_t bytes{1024};
+    auto data{rng.randbytes<std::byte>(bytes)};
+
+    constexpr size_t key_size{sizeof(uint64_t)};
+    std::array<std::byte, key_size> obfuscation{};
+    rng.fillrand(obfuscation);
+
+    size_t offset{0};
+    bench.batch(data.size()).unit("byte").run([&] {
+        util::Xor(data, obfuscation, offset++); // mutated differently each time
+        ankerl::nanobench::doNotOptimizeAway(data);
+    });
+}
+
+BENCHMARK(ObfuscationBench, benchmark::PriorityLevel::HIGH);
