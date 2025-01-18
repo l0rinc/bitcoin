871bba6f2379861d9de2a57a0e709d277264303a
Lőrinc <pap.lorinc@gmail.com>
2025-01-18 11:43:26 +0100

    bench: measure `CheckBlock` speed separately from serialization
    |            ns/block |             block/s |    err% |     total | benchmark
    |--------------------:|--------------------:|--------:|----------:|:----------
    |          269,888.41 |            3,705.24 |    0.9% |     10.90 | `CheckBlockBench`

--- src/bench/checkblock.cpp
+++ src/bench/checkblock.cpp
@@ -25,7 +25,7 @@
 // a block off the wire, but before we can relay the block on to peers using
 // compact block relay.
 
-static void DeserializeBlockTest(benchmark::Bench& bench)
+static void DeserializeBlockBench(benchmark::Bench& bench)
 {
     DataStream stream(benchmark::data::block413567);
     std::byte a{0};
@@ -39,26 +39,57 @@ static void DeserializeBlockTest(benchmark::Bench& bench)
     });
 }
 
-static void DeserializeAndCheckBlockTest(benchmark::Bench& bench)
+static std::map<std::string, uint64_t> TallyScriptTypes(const CBlock& block)
 {
-    DataStream stream(benchmark::data::block413567);
-    std::byte a{0};
-    stream.write({&a, 1}); // Prevent compaction
+    auto classify{[&](const CScript& script) {
+        switch (std::vector<std::vector<uint8_t>> s; Solver(script, s)) {
+        case TxoutType::ANCHOR: return "P2A";
+        case TxoutType::MULTISIG: return "P2MS";
+        case TxoutType::NULL_DATA: return "OP_RETURN";
+        case TxoutType::PUBKEY: return "P2PK";
+        case TxoutType::PUBKEYHASH: return "P2PKH";
+        case TxoutType::SCRIPTHASH: return "P2SH";
+        case TxoutType::WITNESS_V1_TAPROOT: return "P2TR";
+        case TxoutType::WITNESS_V0_KEYHASH: return "P2WPKH";
+        case TxoutType::WITNESS_V0_SCRIPTHASH: return "P2WSH";
+        case TxoutType::NONSTANDARD:     // fall-through
+        case TxoutType::WITNESS_UNKNOWN: // fall-through
+        default: return "OTHER";
+        }
+    }};
 
-    ArgsManager bench_args;
-    const auto chainParams = CreateChainParams(bench_args, ChainType::MAIN);
+    std::map<std::string, uint64_t> tally;
+    for (const auto& tx : block.vtx) {
+        for (const auto& txin : tx->vin) ++tally[classify(txin.scriptSig)];
+        for (const auto& txout : tx->vout) ++tally[classify(txout.scriptPubKey)];
+    }
+    return tally;
+}
 
+static void CheckBlockBench(benchmark::Bench& bench)
+{
+    CBlock block;
+    DataStream(benchmark::data::block413567) >> TX_WITH_WITNESS(block);
+
+    // Document what we're actually measuring
+    assert(block.hashMerkleRoot == uint256{"64a50c649fc816baaa2effda230c39cacf1504e4e616a2863685b72aaa7dce05"});
+    assert(block.vtx.size() == 1557);
+    assert(TallyScriptTypes(block) == (std::map<std::string, uint64_t>{
+        {"OP_RETURN", 3},
+        {"OTHER", 4887},
+        {"P2PKH", 2841},
+        {"P2SH", 737},
+    }));
+    /////////////////////////////////////////
+
+    const auto chainParams = CreateChainParams(ArgsManager{}, ChainType::MAIN);
     bench.unit("block").run([&] {
-        CBlock block; // Note that CBlock caches its checked state, so we need to recreate it here
-        stream >> TX_WITH_WITNESS(block);
-        bool rewound = stream.Rewind(benchmark::data::block413567.size());
-        assert(rewound);
-
+        block.fChecked = block.m_checked_witness_commitment = block.m_checked_merkle_root = false; // Reset the cached state
         BlockValidationState validationState;
-        bool checked = CheckBlock(block, validationState, chainParams->GetConsensus());
-        assert(checked);
+        bool checked = CheckBlock(block, validationState, chainParams->GetConsensus(), /*fCheckPOW=*/true, /*fCheckMerkleRoot=*/true);
+        assert(checked && validationState.IsValid());
     });
 }
 
-BENCHMARK(DeserializeBlockTest, benchmark::PriorityLevel::HIGH);
-BENCHMARK(DeserializeAndCheckBlockTest, benchmark::PriorityLevel::HIGH);
+BENCHMARK(DeserializeBlockBench, benchmark::PriorityLevel::HIGH);
+BENCHMARK(CheckBlockBench, benchmark::PriorityLevel::HIGH);
