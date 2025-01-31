34e3ee841b7eb6839284316cc87854efe32b043e
willcl-ark <will@256k1.dev>
2025-01-31 15:10:04 +0000

    print assumeutxosync to console

--- bench-ci/run-assumeutxo-bench.sh
+++ bench-ci/run-assumeutxo-bench.sh
@@ -64,8 +64,8 @@ prepare_assumeutxo_snapshot_run() {
   clean_datadir "${TMP_DATADIR}"
   # Use the pre-built binaries from BINARIES_DIR
   "${BINARIES_DIR}/${commit}/bitcoind" --help
-  taskset -c 0-15 "${BINARIES_DIR}/${commit}/bitcoind" -datadir="${TMP_DATADIR}" -connect="${CONNECT_ADDRESS}" -daemon=0 -chain="${CHAIN}" -stopatheight=1 -printtoconsole=0
-  taskset -c 0-15 "${BINARIES_DIR}/${commit}/bitcoind" -datadir="${TMP_DATADIR}" -connect="${CONNECT_ADDRESS}" -daemon=0 -chain="${CHAIN}" -dbcache="${DBCACHE}" -pausebackgroundsync=1 -loadutxosnapshot="${UTXO_PATH}" -printtoconsole=0 || true
+  taskset -c 0-15 "${BINARIES_DIR}/${commit}/bitcoind" -datadir="${TMP_DATADIR}" -connect="${CONNECT_ADDRESS}" -daemon=0 -chain="${CHAIN}" -stopatheight=1 -printtoconsole=1
+  taskset -c 0-15 "${BINARIES_DIR}/${commit}/bitcoind" -datadir="${TMP_DATADIR}" -connect="${CONNECT_ADDRESS}" -daemon=0 -chain="${CHAIN}" -dbcache="${DBCACHE}" -pausebackgroundsync=1 -loadutxosnapshot="${UTXO_PATH}" -printtoconsole=1 || true
   clean_logs "${TMP_DATADIR}"
 }
 
