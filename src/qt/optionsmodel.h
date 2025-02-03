e1d2b30cdce9f86ea713486035d574040c33f590
Luke Dashjr <luke-jr+git@utopios.org>
2025-02-03 23:43:26 +0000

    Qt/Options: Configure blockmaxsize, blockprioritysize, and blockmaxweight using rwconf
    (cherry picked from commit 044bde585c5194476647ca902051e838baed260f)
    
    https://github.com/bitcoinknots/bitcoin/commit/044bde585c5194476647ca902051e838baed260f

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -94,6 +94,9 @@ public:
         limitdescendantsize,
         rejectbaremultisig,     // bool
         datacarriersize,
+        blockmaxsize,
+        blockprioritysize,
+        blockmaxweight,
         OptionIDRowCount,
     };
 
