e95f3a4396a753c7e1d83900ffa0ddd380831fb3
Luke Dashjr <luke-jr+git@utopios.org>
2025-02-03 23:32:39 +0000

    Qt/Options: Configure limitdescendant{count,size} using rwconf
    (cherry picked from commit c763d7b82de13647d2b4c623e08a1eeaf62c8289)
    
    https://github.com/bitcoinknots/bitcoin/commit/c763d7b82de13647d2b4c623e08a1eeaf62c8289

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -90,6 +90,8 @@ public:
         bytespersigopstrict,
         limitancestorcount,
         limitancestorsize,
+        limitdescendantcount,
+        limitdescendantsize,
         OptionIDRowCount,
     };
 
