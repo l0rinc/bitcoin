817fef3e682ecd996dd746e3fbf7b47a61e2122c
Luke Dashjr <luke-jr+git@utopios.org>
2025-02-04 00:02:27 +0000

    Qt/Options: Configure dustrelayfee using rwconf
    (cherry picked from commit 10f8de3bb7b83a3034c28f29a2d48d202318d558)
    
    https://github.com/bitcoinknots/bitcoin/commit/10f8de3bb7b83a3034c28f29a2d48d202318d558

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -97,6 +97,7 @@ public:
         limitdescendantsize,
         rejectbaremultisig,     // bool
         datacarriersize,
+        dustrelayfee,
         blockmaxsize,
         blockprioritysize,
         blockmaxweight,
