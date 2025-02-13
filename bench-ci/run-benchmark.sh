aa94b6074e2e8e843b360b793f73b5a5d7cc5c41
willcl-ark <will@256k1.dev>
2025-02-13 13:19:50 +0000

    add uninstrumented run

--- bench-ci/run-benchmark.sh
+++ bench-ci/run-benchmark.sh
@@ -44,7 +44,6 @@ setup_run() {
   set -euxo pipefail
 
   local TMP_DATADIR="$1"
-  local commit="$2"
   clean_datadir "${TMP_DATADIR}"
 }
 
@@ -85,17 +84,15 @@ run_benchmark() {
   local TMP_DATADIR="$3"
   local ORIGINAL_DATADIR="$4"
   local results_file="$5"
-  local png_dir="$6"
-  local chain="$7"
-  local stop_at_height="$8"
-  local connect_address="$9"
-  local dbcache="${10}"
-  local BINARIES_DIR="${11}"
+  local chain="$6"
+  local stop_at_height="$7"
+  local connect_address="$8"
+  local dbcache="${9}"
+  local BINARIES_DIR="${10}"
 
   # Export functions so they can be used by hyperfine
   export -f setup_run
   export -f prepare_run
-  export -f conclude_run
   export -f cleanup_run
   export -f clean_datadir
   export -f clean_logs
@@ -108,33 +105,31 @@ run_benchmark() {
   echo "base_commit: ${base_commit}"
   echo "head_commit: ${head_commit}"
   echo "results_file: ${results_file}"
-  echo "png_dir: ${png_dir}"
   echo "chain: ${chain}"
   echo "stop_at_height: ${stop_at_height}"
   echo "connect_address: ${connect_address}"
   echo "dbcache: ${dbcache}"
-  echo "\n"
+  printf \n
 
   # Run hyperfine
   hyperfine \
     --shell=bash \
-    --setup "setup_run ${TMP_DATADIR} {commit}" \
+    --setup "setup_run ${TMP_DATADIR}" \
     --prepare "prepare_run ${TMP_DATADIR} ${ORIGINAL_DATADIR}" \
-    --conclude "conclude_run {commit} ${TMP_DATADIR} ${png_dir}" \
     --cleanup "cleanup_run ${TMP_DATADIR}" \
     --runs 3 \
     --export-json "${results_file}" \
     --show-output \
     --command-name "base (${base_commit})" \
     --command-name "head (${head_commit})" \
-    "taskset -c 2-15 chrt -o 1 ${BINARIES_DIR}/{commit}/bitcoind -datadir=${TMP_DATADIR} -connect=${connect_address} -daemon=0 -prune=10000 -chain=${chain} -stopatheight=${stop_at_height} -dbcache=${dbcache} -printtoconsole=0" \
+    "taskset -c 2-15 chrt -o 0 ${BINARIES_DIR}/{commit}/bitcoind -datadir=${TMP_DATADIR} -connect=${connect_address} -daemon=0 -prune=10000 -chain=${chain} -stopatheight=${stop_at_height} -dbcache=${dbcache} -printtoconsole=0" \
     -L commit "base,head"
 }
 
 # Main execution
-if [ "$#" -ne 11 ]; then
-  echo "Usage: $0 base_commit head_commit TMP_DATADIR ORIGINAL_DATADIR results_dir png_dir chain stop_at_height connect_address dbcache BINARIES_DIR"
+if [ "$#" -ne 10 ]; then
+  echo "Usage: $0 base_commit head_commit TMP_DATADIR ORIGINAL_DATADIR results_dir chain stop_at_height connect_address dbcache BINARIES_DIR"
   exit 1
 fi
 
-run_benchmark "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}" "${11}"
+run_benchmark "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "${9}" "${10}"
