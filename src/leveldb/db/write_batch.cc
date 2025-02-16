64d0224c536c1596457b64ed2534489f913b05ca
Lőrinc <pap.lorinc@gmail.com>
2025-02-17 00:42:47 +0100

    WriteBatch::Reserve

--- src/leveldb/db/write_batch.cc
+++ src/leveldb/db/write_batch.cc
@@ -95,6 +95,10 @@ void WriteBatchInternal::SetSequence(WriteBatch* b, SequenceNumber seq) {
   EncodeFixed64(&b->rep_[0], seq);
 }
 
+void WriteBatch::Reserve(size_t size) {
+  rep_.reserve(size);
+}
+
 void WriteBatch::Put(const Slice& key, const Slice& value) {
   WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
   rep_.push_back(static_cast<char>(kTypeValue));
