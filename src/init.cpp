cf068489dad5d2138f15ae71b024c523585a9d11
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-03-08 02:08:37 +0100

    SwiftSync: add proof-of-concept implementation
    - reads in SwiftSync hints bitmap file at startup (hardcoded path by now)
    - skips basically all checks in ConnectBlock() that depend on prevout information
    - check that aggregated hash is empty after the terminal swiftsync block

--- src/init.cpp
+++ src/init.cpp
@@ -69,6 +69,7 @@
 #include <rpc/util.h>
 #include <scheduler.h>
 #include <script/sigcache.h>
+#include <swiftsync.h>
 #include <sync.h>
 #include <torcontrol.h>
 #include <txdb.h>
@@ -147,6 +148,8 @@ static constexpr bool DEFAULT_PROXYRANDOMIZE{true};
 static constexpr bool DEFAULT_REST_ENABLE{false};
 static constexpr bool DEFAULT_I2P_ACCEPT_INCOMING{true};
 static constexpr bool DEFAULT_STOPAFTERBLOCKIMPORT{false};
+
+extern SwiftSyncHints g_swiftsync_hints;
 
 #ifdef WIN32
 // Win32 LevelDB doesn't use filedescriptors, and the ones used for
@@ -1764,6 +1767,9 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
         validation_signals.RegisterValidationInterface(g_zmq_notification_interface.get());
     }
 #endif
+
+    LogInfo("Loading SwiftSync hints bitmap...");
+    g_swiftsync_hints.Load("./booster.bin"); // TODO: add -swiftsyncfile parameter
 
     // ********************************************************* Step 7: load block chain
 
