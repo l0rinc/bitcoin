06986c072206f66f54906b6108185c3bbbb88869
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2025-04-04 15:55:49 +0200

    common: Add GetFreeRAM function

--- src/common/system.h
+++ src/common/system.h
@@ -9,6 +9,7 @@
 #include <bitcoin-build-config.h> // IWYU pragma: keep
 
 #include <cstdint>
+#include <optional>
 #include <string>
 
 // Application startup time (used for uptime calculation)
@@ -28,5 +29,7 @@ void runCommand(const std::string& strCommand);
  * @note This does count virtual cores, such as those provided by HyperThreading.
  */
 int GetNumCores();
+
+std::optional<size_t> GetFreeRAM();
 
 #endif // BITCOIN_COMMON_SYSTEM_H
