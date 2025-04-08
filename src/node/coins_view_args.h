aa4568c1b42cafa8cef6847a78b1d1c0fe46da9e
Lőrinc <pap.lorinc@gmail.com>
2025-04-08 17:49:19 +0200

    Add `GetDbBatchSize` to calculate target batch size based on dbcache value
    For default `-dbcache` (450MiB) and lower the previous write batch size was preserved (16MiB), scaling linearly, capped at 256MiB batch sizes for predictability.

--- src/node/coins_view_args.h
+++ src/node/coins_view_args.h
@@ -5,11 +5,24 @@
 #ifndef BITCOIN_NODE_COINS_VIEW_ARGS_H
 #define BITCOIN_NODE_COINS_VIEW_ARGS_H
 
+#include <node/caches.h>
+
 class ArgsManager;
 struct CoinsViewOptions;
 
-namespace node {
-void ReadCoinsViewArgs(const ArgsManager& args, CoinsViewOptions& options);
+namespace node
+{
+    static constexpr size_t MIN_DB_CACHE_BATCH{16_MiB};
+    static constexpr size_t DEFAULT_DB_CACHE_BATCH{16_MiB}; // The batch size of DEFAULT_KERNEL_CACHE
+    static constexpr size_t MAX_DB_CACHE_BATCH{256_MiB};
+
+    static constexpr size_t GetDbBatchSize(const size_t dbcache_bytes)
+    {
+        const size_t target{(dbcache_bytes / DEFAULT_KERNEL_CACHE) * DEFAULT_DB_CACHE_BATCH};
+        return std::max<size_t>(MIN_DB_CACHE_BATCH, std::min<size_t>(MAX_DB_CACHE_BATCH, target));
+    }
+
+    void ReadCoinsViewArgs(const ArgsManager& args, CoinsViewOptions& options);
 } // namespace node
 
 #endif // BITCOIN_NODE_COINS_VIEW_ARGS_H
