ed513ca38b5da26be1abfcb6fc5e11e30c7a057b
Hodlinator <172445034+hodlinator@users.noreply.github.com>
2025-04-17 11:58:04 +0200

    headerssync: Skip commitments for cached headers
    As we cache rather than redownload them, it doesn't make sense to generate commitments for those and then verify against the same data.
    
    Also skip duplicate check for PermittedDifficultyTransition during REDOWNLOAD as we already did it during PRESYNC for cached headers.

--- src/headerssync.cpp
+++ src/headerssync.cpp
@@ -65,7 +65,8 @@ HeadersSyncState::HeadersSyncState(NodeId id, const Consensus::Params& consensus
     // exceeds this bound, because it's not possible for a consensus-valid
     // chain to be longer than this (at the current time -- in the future we
     // could try again, if necessary, to sync a longer chain).
-    m_max_commitments = 6*(Ticks<std::chrono::seconds>(NodeClock::now() - NodeSeconds{std::chrono::seconds{chain_start->GetMedianTimePast()}}) + MAX_FUTURE_BLOCK_TIME) / HEADER_COMMITMENT_PERIOD;
+    // Subtract cached headers as we don't redownload and verify commitments for them.
+    m_max_commitments = std::max<int64_t>(0, (6*(Ticks<std::chrono::seconds>(NodeClock::now() - NodeSeconds{std::chrono::seconds{chain_start->GetMedianTimePast()}}) + MAX_FUTURE_BLOCK_TIME) - m_headers_cache.capacity()) / HEADER_COMMITMENT_PERIOD);
 
     LogDebug(BCLog::NET, "Initial headers sync started with peer=%d: height=%i, max_commitments=%i, min_work=%s, cache=%d headers (%.1f MiB)", m_id, m_presync_height, m_max_commitments, m_minimum_required_work.ToString(), m_headers_cache.capacity(), (m_headers_cache.capacity() * sizeof(decltype(m_headers_cache)::value_type)) / (1024.0f * 1024.0f));
 }
@@ -178,7 +179,7 @@ HeadersSyncState::ProcessingResult HeadersSyncState::ProcessRedownload(const
     // we'll return a batch of headers to the caller for processing.
     ret.success = true;
     for (const auto& hdr : received_headers) {
-        if (!ValidateAndStoreRedownloadedHeader(hdr)) {
+        if (!ValidateAndStoreRedownloadedHeader(hdr, /*from_cache=*/false)) {
             // Something went wrong -- the peer gave us an unexpected chain.
             // We could consider looking at the reason for failure and
             // punishing the peer, but for now just give up on sync.
@@ -249,7 +250,7 @@ bool HeadersSyncState::ValidateAndStoreHeadersCommitments(const std::vector<CBlo
             uint256 prev_hash{m_chain_start->GetBlockHash()};
             for (const auto& compressed_header : m_headers_cache) {
                 const CBlockHeader header{compressed_header.GetFullHeader(prev_hash)};
-                if (!ValidateAndStoreRedownloadedHeader(header)) {
+                if (!ValidateAndStoreRedownloadedHeader(header, /*from_cache=*/true)) {
                     return false;
                 }
                 prev_hash = header.GetHash();
@@ -285,7 +286,10 @@ bool HeadersSyncState::ValidateAndProcessSingleHeader(const CBlockHeader& curren
         return false;
     }
 
-    if (next_height % HEADER_COMMITMENT_PERIOD == m_commit_offset) {
+    if (m_headers_cache.size() < m_headers_cache.capacity()) {
+        // We do not redownload cached entries so we skip storing commitments for them.
+        m_headers_cache.emplace_back(current);
+    } else if (next_height % HEADER_COMMITMENT_PERIOD == m_commit_offset) {
         // Add a commitment.
         m_header_commitments.push_back(m_hasher(current.GetHash()) & 1);
         if (m_header_commitments.size() > m_max_commitments) {
@@ -302,14 +306,10 @@ bool HeadersSyncState::ValidateAndProcessSingleHeader(const CBlockHeader& curren
     m_presync_last_header_received = current;
     m_presync_height = next_height;
 
-    if (m_headers_cache.size() < m_headers_cache.capacity()) {
-        m_headers_cache.emplace_back(current);
-    }
-
     return true;
 }
 
-bool HeadersSyncState::ValidateAndStoreRedownloadedHeader(const CBlockHeader& header)
+bool HeadersSyncState::ValidateAndStoreRedownloadedHeader(const CBlockHeader& header, bool from_cache)
 {
     Assume(m_state == State::REDOWNLOAD);
     if (m_state != State::REDOWNLOAD) return false;
@@ -319,6 +319,7 @@ bool HeadersSyncState::ValidateAndStoreRedownloadedHeader(const CBlockHeader& he
     // Ensure that we're working on a header that connects to the chain we're
     // downloading.
     if (header.hashPrevBlock != m_redownload_buffer_last_hash) {
+        assert(!from_cache); // Cache should only contain contiguous headers.
         LogDebug(BCLog::NET, "Initial headers sync aborted with peer=%d: non-continuous headers at height=%i (redownload phase)\n", m_id, next_height);
         return false;
     }
@@ -331,8 +332,8 @@ bool HeadersSyncState::ValidateAndStoreRedownloadedHeader(const CBlockHeader& he
         previous_nBits = m_chain_start->nBits;
     }
 
-    if (!PermittedDifficultyTransition(m_consensus_params, next_height,
-                previous_nBits, header.nBits)) {
+    if (!from_cache && !PermittedDifficultyTransition(m_consensus_params, next_height,
+                                                      previous_nBits, header.nBits)) {
         LogDebug(BCLog::NET, "Initial headers sync aborted with peer=%d: invalid difficulty transition at height=%i (redownload phase)\n", m_id, next_height);
         return false;
     }
@@ -350,7 +351,7 @@ bool HeadersSyncState::ValidateAndStoreRedownloadedHeader(const CBlockHeader& he
     // it's possible our peer has extended its chain between our first sync and
     // our second, and we don't want to return failure after we've seen our
     // target blockhash just because we ran out of commitments.
-    if (!m_process_all_remaining_headers && next_height % HEADER_COMMITMENT_PERIOD == m_commit_offset) {
+    if (!m_process_all_remaining_headers && !from_cache && next_height % HEADER_COMMITMENT_PERIOD == m_commit_offset) {
         if (m_header_commitments.size() == 0) {
             LogDebug(BCLog::NET, "Initial headers sync aborted with peer=%d: commitment overrun at height=%i (redownload phase)\n", m_id, next_height);
             // Somehow our peer managed to feed us a different chain and
