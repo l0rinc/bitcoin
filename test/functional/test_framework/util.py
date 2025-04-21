522cb1e1734bb2acfd801ee4ac99fa743d3ec7c0
Luke Dashjr <luke-jr+git@utopios.org>
2025-04-21 20:14:42 +0000

    Revert "daemon: remove UPnP support"
    This reverts commit 038bbe7b20074cc2201585dcc631e81b9e1e306c.
    
    (cherry picked from commit 185b16db9277243bc8d6fc2a6e751d5faf0f0bb3)

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -560,7 +560,9 @@ def write_config(config_path, *, n, chain, extra_config="", disable_autoconnect=
         # in tests.
         f.write("peertimeout=999999999\n")
         f.write("printtoconsole=0\n")
-        f.write("natpmp=0\n") # Avoid non-loopback network traffic during tests.
+        # Avoid non-loopback network traffic during tests.
+        f.write("upnp=0\n")
+        f.write("natpmp=0\n")
         f.write("shrinkdebugfile=0\n")
         # To improve SQLite wallet performance so that the tests don't timeout, use -unsafesqlitesync
         f.write("unsafesqlitesync=1\n")
