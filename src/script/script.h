353474db8aede428cd55fc99428de02830568cc9
Lőrinc <pap.lorinc@gmail.com>
2025-05-03 21:31:12 +0200

    Reserve blocks - needs new swiftsync file

--- src/script/script.h
+++ src/script/script.h
@@ -608,8 +608,6 @@ public:
 
     bool IsWitnessProgram(int& version, std::vector<unsigned char>& program) const;
 
-    bool IsPayToTaproot() const;
-
     /** Called by IsStandardTx and P2SH/BIP62 VerifyScript (which makes it consensus-critical). */
     bool IsPushOnly(const_iterator pc) const;
     bool IsPushOnly() const;
