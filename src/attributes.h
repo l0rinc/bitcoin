acdc4a331eeab5025a28ff5c896db6ab57049263
Lőrinc <pap.lorinc@gmail.com>
2025-05-22 11:45:15 +0200

    experiment: unroll only

--- src/attributes.h
+++ src/attributes.h
@@ -16,12 +16,27 @@
 #  define LIFETIMEBOUND
 #endif
 
-#if defined(__GNUC__)
+#if defined(__GNUC__) // Covers GCC and Clang
 #  define ALWAYS_INLINE inline __attribute__((always_inline))
 #elif defined(_MSC_VER)
 #  define ALWAYS_INLINE __forceinline
 #else
 #  error No known always_inline attribute for this platform.
+#endif
+
+#if defined(__GNUC__) && !defined(__clang__)
+#  define FORCE_O3 [[gnu::optimize("-O3")]]
+#  define OPTIMIZED(func_def) \
+FORCE_O3 func_def
+#elif defined(__clang__)
+#  define FORCE_O3
+#  define OPTIMIZED(func_def) \
+_Pragma("clang optimize on") \
+func_def \
+_Pragma("clang optimize off")
+#else
+#  define FORCE_O3
+#  define OPTIMIZED(func_def) func_def
 #endif
 
 #endif // BITCOIN_ATTRIBUTES_H
