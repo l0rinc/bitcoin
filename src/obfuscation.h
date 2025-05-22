9472af65af1b6759b2860c973b774aaa6939d4f4
Lőrinc <pap.lorinc@gmail.com>
2025-05-22 11:45:15 +0200

    experiment: unroll only

--- src/obfuscation.h
+++ src/obfuscation.h
@@ -26,15 +26,19 @@ public:
     void operator()(std::span<std::byte> target, const size_t key_offset_bytes = 0) const
     {
         if (!*this) return;
-        uint64_t rot_key{m_rotations[key_offset_bytes % SIZE_BYTES]}; // Continue obfuscation from where we left off
-        const size_t alignment_remaining{std::min(SIZE_BYTES - (reinterpret_cast<ptrdiff_t>(target.data()) % SIZE_BYTES), target.size())};
-        Xor(target, rot_key, alignment_remaining);
-        target = target.subspan(alignment_remaining);
-        rot_key = m_rotations[(key_offset_bytes + alignment_remaining) % SIZE_BYTES];
-        for (; target.size() >= SIZE_BYTES; target = target.subspan(SIZE_BYTES)) { // Process multiple bytes at a time
-            *reinterpret_cast<uint64_t*>(target.data()) ^= rot_key;
+        const uint64_t rot_key{m_rotations[key_offset_bytes % SIZE_BYTES]}; // Continue obfuscation from where we left off
+
+        // Process multiple bytes at a time
+        for (constexpr auto unroll{8}; target.size() >= SIZE_BYTES * unroll; target = target.subspan(SIZE_BYTES * unroll)) {
+            for (size_t i{0}; i < unroll; ++i) {
+                Xor(target.subspan(i * SIZE_BYTES, SIZE_BYTES), rot_key, SIZE_BYTES);
+            }
         }
-        Xor(target, rot_key, target.size());
+        while (!target.empty()) {
+            const size_t chunk = std::min(SIZE_BYTES, target.size());
+            Xor(target.first(chunk), rot_key, chunk);
+            target = target.subspan(chunk);
+        }
     }
 
     template <typename Stream>
