e3a3abd07ba8a1e82fd7bf03013b82aa01f84042
benthecarman <benthecarman@live.com>
2025-05-23 17:28:26 -0500

    rpc: Note in fundrawtransaction doc, fee rate is for package
    Github-Pull: #32607
    Rebased-From: f98e1aaf34e347088caa54403521e3b5cb55dd40
    (cherry picked from commit c899334e36b63b530c44fe7273c951469762878c)

--- src/wallet/rpc/spend.cpp
+++ src/wallet/rpc/spend.cpp
@@ -864,6 +864,7 @@ RPCMethod fundrawtransaction()
                 "Note that all inputs selected must be of standard form and P2SH scripts must be\n"
                 "in the wallet using importdescriptors (to calculate fees).\n"
                 "You can see whether this is the case by checking the \"solvable\" field in the listunspent output.\n"
+                "Only pay-to-pubkey, multisig, and P2SH versions thereof are currently supported for watch-only.\n"
                 "Note that if specifying an exact fee rate, the resulting transaction may have a higher fee rate\n"
                 "if the transaction has unconfirmed inputs. This is because the wallet will attempt to make the\n"
                 "entire package have the given fee rate, not the resulting transaction.\n",
