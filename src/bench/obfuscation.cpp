72c775d58a70771efa48acf12da7a6d434adf04e
Lőrinc <pap.lorinc@gmail.com>
2025-07-05 13:39:15 +0200

    refactor: move `util::Xor` to `Obfuscation().Xor`
    This is meant to focus the usages to narrow the scope of the obfuscation optimization.
    
    `Obfuscation::Xor` is mostly a move, except that the key size is fixed to `KEY_SIZE`, since that was the only size used anyway. This will be important in later optimizations.
    
    Co-authored-by: maflcko <6399679+maflcko@users.noreply.github.com>

--- src/bench/obfuscation.cpp
+++ src/bench/obfuscation.cpp
@@ -3,9 +3,9 @@
 // file COPYING or https://opensource.org/license/mit/.
 
 #include <bench/bench.h>
+#include <obfuscation.h>
 #include <random.h>
 #include <span.h>
-#include <streams.h>
 
 #include <cstddef>
 #include <vector>
@@ -15,11 +15,11 @@ static void ObfuscationBench(benchmark::Bench& bench)
     FastRandomContext frc{/*fDeterministic=*/true};
     auto data{frc.randbytes<std::byte>(1024)};
 
-    const auto key_bytes{frc.randbytes<sizeof(uint64_t)>()};
+    const auto key_bytes{frc.randbytes<Obfuscation::KEY_SIZE>()};
 
     size_t offset{0};
     bench.batch(data.size()).unit("byte").run([&] {
-        util::Xor(data, key_bytes, offset++); // mutated differently each time
+        Obfuscation().Xor(data, key_bytes, offset++); // mutated differently each time
         ankerl::nanobench::doNotOptimizeAway(data);
     });
 }
