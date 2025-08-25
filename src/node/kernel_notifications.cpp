7d6a9f85f3bce25191c34a1732a91ddcfc7b6355
Luke Dashjr <luke-jr+git@utopios.org>
2025-08-25 22:23:24 +0000

    Bugfix: notifications: Use double-quote instead of single-quote
    Windows cmd.exe does not interpret single quotes as quoting characters
    
    (cherry picked from commit 99bd4320f8f814a01409762555036744ddbe63c9)

--- src/node/kernel_notifications.cpp
+++ src/node/kernel_notifications.cpp
@@ -35,7 +35,7 @@ static void AlertNotify(const std::string& strMessage)
     // Alert text should be plain ascii coming from a trusted source, but to
     // be safe we first strip anything not in safeChars, then add single quotes around
     // the whole string before passing it to the shell:
-    std::string singleQuote("'");
+    std::string singleQuote("\"");
     std::string safeStatus = SanitizeString(strMessage);
     safeStatus = singleQuote+safeStatus+singleQuote;
 
