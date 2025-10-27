c9ee67745e2d49337f21468e4b2c3a4925c2a53c
Lőrinc <pap.lorinc@gmail.com>
2025-10-27 15:14:13 +0100

    Revert "Slice::compare"
    This reverts commit 6fb64b34a83b941c4f62b81082d9f664512da55a.

--- src/leveldb/include/leveldb/slice.h
+++ src/leveldb/include/leveldb/slice.h
@@ -102,8 +102,15 @@ inline bool operator==(const Slice& x, const Slice& y) {
 inline bool operator!=(const Slice& x, const Slice& y) { return !(x == y); }
 
 inline int Slice::compare(const Slice& b) const {
-  if (int r{memcmp(data_, b.data_, std::min(size_, b.size_))}; r != 0 || size_ == b.size_) return r;
-  return (size_ < b.size_) ? -1 : 1;
+  const size_t min_len = (size_ < b.size_) ? size_ : b.size_;
+  int r = memcmp(data_, b.data_, min_len);
+  if (r == 0) {
+    if (size_ < b.size_)
+      r = -1;
+    else if (size_ > b.size_)
+      r = +1;
+  }
+  return r;
 }
 
 }  // namespace leveldb
