23e89b87fea543d5028b846d706f55db14cd2796
Lőrinc <pap.lorinc@gmail.com>
2025-11-27 22:44:43 +0100

    don't reallocate cache during IBD Flush

--- src/coins.cpp
+++ src/coins.cpp
@@ -249,14 +249,12 @@ bool CCoinsViewCache::BatchWrite(CoinsViewCacheCursor& cursor, const uint256 &ha
     return true;
 }
 
-bool CCoinsViewCache::Flush(bool will_reuse_cache) {
+bool CCoinsViewCache::Flush(bool deallocate) {
     auto cursor{CoinsViewCacheCursor(m_sentinel, cacheCoins, /*will_erase=*/true)};
     bool fOk = base->BatchWrite(cursor, hashBlock);
     if (fOk) {
         cacheCoins.clear();
-        if (will_reuse_cache) {
-            ReallocateCache();
-        }
+        if (deallocate) ReallocateCache();
         cachedCoinsUsage = 0;
     }
     return fOk;
