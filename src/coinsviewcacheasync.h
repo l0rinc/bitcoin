042dbfdc3f2c2ea5f04dfa91ac8785a42d493c2f
Lőrinc <pap.lorinc@gmail.com>
2025-12-03 21:58:22 +0100

    WORKER_THREADS{2}

--- src/coinsviewcacheasync.h
+++ src/coinsviewcacheasync.h
@@ -23,7 +23,7 @@
 #include <utility>
 #include <vector>
 
-static constexpr int32_t WORKER_THREADS{1};
+static constexpr int32_t WORKER_THREADS{2};
 
 /**
  * CCoinsViewCache subclass that asynchronously fetches block inputs in parallel.
