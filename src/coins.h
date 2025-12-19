9930a960747f8db26231f964ffbbea32b4a7c125
Lőrinc <pap.lorinc@gmail.com>
2025-12-19 13:26:14 +0100

    refactor: make `CCoinsView::Cursor` purely virtual

--- src/coins.h
+++ src/coins.h
@@ -324,7 +324,7 @@ public:
     virtual void BatchWrite(CoinsViewCacheCursor& cursor, const uint256& hash_block) = 0;
 
     //! Get a cursor to iterate over the whole state
-    virtual std::unique_ptr<CCoinsViewCursor> Cursor() const;
+    virtual std::unique_ptr<CCoinsViewCursor> Cursor() const = 0;
 
     //! Estimate database size
     virtual size_t EstimateSize() const = 0;
@@ -352,6 +352,7 @@ public:
     {
         for (auto it{cursor.Begin()}; it != cursor.End(); it = cursor.NextAndMaybeErase(*it)) { }
     }
+    std::unique_ptr<CCoinsViewCursor> Cursor() const override { return {}; }
     size_t EstimateSize() const override { return 0; }
 };
 
