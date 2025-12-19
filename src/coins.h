0000136ec360f9e6ed8def02e3a2ab86deacd049
Lőrinc <pap.lorinc@gmail.com>
2025-12-19 13:34:40 +0100

    refactor: make `CCoinsView::GetHeadBlocks` purely virtual

--- src/coins.h
+++ src/coins.h
@@ -317,7 +317,7 @@ public:
     //! If the database is in a consistent state, the result is the empty vector.
     //! Otherwise, a two-element vector is returned consisting of the new and
     //! the old block hash, in that order.
-    virtual std::vector<uint256> GetHeadBlocks() const;
+    virtual std::vector<uint256> GetHeadBlocks() const = 0;
 
     //! Do a bulk modification (multiple Coin changes + BestBlock change).
     //! The passed cursor is used to iterate through the coins.
@@ -348,6 +348,7 @@ public:
 
     std::optional<Coin> GetCoin(const COutPoint&) const override { return {}; }
     uint256 GetBestBlock() const override { return {}; }
+    std::vector<uint256> GetHeadBlocks() const override { return {}; }
     void BatchWrite(CoinsViewCacheCursor& cursor, const uint256&) override
     {
         for (auto it{cursor.Begin()}; it != cursor.End(); it = cursor.NextAndMaybeErase(*it)) { }
