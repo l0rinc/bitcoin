fab1271b1b9a235eacf1768615eb4736112cb88f
Andrew Toth <andrewstoth@gmail.com>
2025-12-22 17:47:02 -0500

    validation: reuse connect block coins view
    Co-authored-by: l0rinc <pap.lorinc@gmail.com>

--- src/coins.h
+++ src/coins.h
@@ -457,6 +457,9 @@ public:
      */
     bool Sync();
 
+    //! Wipe local state.
+    void Reset() noexcept;
+
     /**
      * Removes the UTXO with the given outpoint from the cache, if it is
      * not modified.
