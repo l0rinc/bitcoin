cfb92655d84670657c18395d98bfbc51318580fd
Lőrinc <pap.lorinc@gmail.com>
2026-01-22 14:53:34 +0100

    dummy: no-op (3/10)

--- src/util/check.h
+++ src/util/check.h
@@ -15,7 +15,7 @@
 #include <string_view>
 #include <utility>
 
-static_assert(true, "CI demo: commit #2");
+static_assert(true, "CI demo: commit #3");
 
 constexpr bool G_FUZZING_BUILD{
 #ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
