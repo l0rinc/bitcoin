b33fea381a9f98bdbcfe6c26f0453fe4ac11d1e0
Lőrinc <pap.lorinc@gmail.com>
2026-02-16 18:45:09 +0000

    serialize: move map/set elements into hinted inserts
    Use std::move when inserting deserialized temporary map/set elements. The value/category is already unique in this context, so this removes extra key/value copies while keeping code straightforward.
    
    Microbenchmark (/tmp/serialize_assoc_perf.cpp, g++-14.2, -O3):
    
    before: /tmp/serialize_assoc_idea27_before.tsv
    
    after:  /tmp/serialize_assoc_idea27_after2.tsv
    
    UnserializeMap: 117092.799167 -> 92975.440833 ns/op (-20.598%)
    
    UnserializeSet: 157473.293333 -> 132820.890000 ns/op (-15.655%)

--- src/serialize.h
+++ src/serialize.h
@@ -960,7 +960,7 @@ void Unserialize(Stream& is, std::map<K, T, Pred, A>& m)
     {
         std::pair<K, T> item;
         Unserialize(is, item);
-        mi = m.insert(mi, item);
+        mi = m.insert(mi, std::move(item));
     }
 }
 
@@ -987,7 +987,7 @@ void Unserialize(Stream& is, std::set<K, Pred, A>& m)
     {
         K key;
         Unserialize(is, key);
-        it = m.insert(it, key);
+        it = m.insert(it, std::move(key));
     }
 }
 
