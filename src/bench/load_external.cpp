tavecchiacoin/tav

bench: route legacy fixture through helper APIs
Bench files loaded `block413567.raw` directly, so moving off the fixture required touching each benchmark's serialization setup.
Add shared helper entry points backed by the current fixture and switch benchmark callsites to those helpers.
This keeps behavior stable while moving setup to `regtest`, where the follow-up generated-block path can find a valid nonce quickly.

Co-authored-by: hodlinator <hodlinator@users.noreply.github.com>

src/bench/load_external.cpp
@@ -3,7 +3,7 @@
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
 #include <bench/bench.h>
-#include <bench/data/block413567.raw.h>
+#include <bench/block_generator.h>
 #include <chainparams.h>
 #include <flatfile.h>
 #include <node/blockstorage.h>
@@ -35,17 +35,18 @@
  */
 static void LoadExternalBlockFile(benchmark::Bench& bench)
 {
-    const auto testing_setup{MakeNoLogFileContext<const TestingSetup>(ChainType::MAIN)};
+    const auto testing_setup{MakeNoLogFileContext<const TestingSetup>(ChainType::REGTEST)};
 
     // Create a single block as in the blocks files (magic bytes, block size,
     // block data) as a stream object.
     const fs::path blkfile{testing_setup.get()->m_path_root / "blk.dat"};
     DataStream ss{};
-    auto params{testing_setup->m_node.chainman->GetParams()};
+    const auto& params{testing_setup->m_node.chainman->GetParams()};
+    const auto block_data{benchmark::GenerateBlockData(params)};
     ss << params.MessageStart();
-    ss << static_cast<uint32_t>(benchmark::data::block413567.size());
+    ss << static_cast<uint32_t>(block_data.size());
     // Use span-serialization to avoid writing the size first.
-    ss << std::span{benchmark::data::block413567};
+    ss << std::span{block_data};
 
     // Create the test file.
     {

l0rinc/bitcoin

Co-authored-by: MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>

src/bench/load_external.cpp
@@ -3,7 +3,7 @@
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
 #include <bench/bench.h>
-#include <bench/data/block413567.raw.h>
+#include <bench/block_generator.h>
 #include <chainparams.h>
 #include <flatfile.h>
 #include <node/blockstorage.h>
@@ -35,17 +35,18 @@
  */
 static void LoadExternalBlockFile(benchmark::Bench& bench)
 {
-    const auto testing_setup{MakeNoLogFileContext<const TestingSetup>(ChainType::MAIN)};
+    const auto testing_setup{MakeNoLogFileContext<const ChainTestingSetup>(ChainType::REGTEST)};
 
     // Create a single block as in the blocks files (magic bytes, block size,
     // block data) as a stream object.
     const fs::path blkfile{testing_setup.get()->m_path_root / "blk.dat"};
     DataStream ss{};
-    auto params{testing_setup->m_node.chainman->GetParams()};
+    const auto& params{testing_setup->m_node.chainman->GetParams()};
+    const auto block_data{benchmark::GenerateBlockData(params)};
     ss << params.MessageStart();
-    ss << static_cast<uint32_t>(benchmark::data::block413567.size());
+    ss << static_cast<uint32_t>(block_data.size());
     // Use span-serialization to avoid writing the size first.
-    ss << std::span{benchmark::data::block413567};
+    ss << std::span{block_data};
 
     // Create the test file.
     {