f2f0fa63ec7fa4bddaca547d42b015109bb18b51
Lőrinc <pap.lorinc@gmail.com>
2026-06-08 14:25:25 +0200

    max_open_files = 2048

--- src/dbwrapper.cpp
+++ src/dbwrapper.cpp
@@ -112,24 +112,20 @@ public:
 };
 
 static void SetMaxOpenFiles(leveldb::Options *options) {
-    // On most platforms the default setting of max_open_files (which is 1000)
-    // is optimal. On Windows using a large file count is OK because the handles
-    // do not interfere with select() loops. On 64-bit Unix hosts this value is
-    // also OK, because up to that amount LevelDB will use an mmap
-    // implementation that does not use extra file descriptors (the fds are
-    // closed after being mmap'ed).
-    //
-    // Increasing the value beyond the default is dangerous because LevelDB will
-    // fall back to a non-mmap implementation when the file count is too large.
-    // On 32-bit Unix host we should decrease the value because the handles use
-    // up real fds, and we want to avoid fd exhaustion issues.
+    // On 64-bit Unix hosts, use a larger table cache so nodes with many small
+    // chainstate files do not repeatedly evict and rebuild table-cache entries
+    // during random UTXO reads. On 32-bit Unix hosts, decrease the value
+    // because the handles use real fds and we want to avoid fd exhaustion
+    // issues.
     //
     // See PR #12495 for further discussion.
 
-    int default_open_files = options->max_open_files;
+    const int default_open_files = options->max_open_files;
 #ifndef WIN32
-    if (sizeof(void*) < 8) {
+    if constexpr (sizeof(void*) < 8) {
         options->max_open_files = 64;
+    } else {
+        options->max_open_files = 2048;
     }
 #endif
     LogDebug(BCLog::LEVELDB, "LevelDB using max_open_files=%d (default=%d)\n",
