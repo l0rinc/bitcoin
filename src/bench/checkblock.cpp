tavecchiacoin/tav

bench: route legacy fixture through helper APIs
Bench files loaded `block413567.raw` directly, so moving off the fixture required touching each benchmark's serialization setup.
Add shared helper entry points backed by the current fixture and switch benchmark callsites to those helpers.
This keeps behavior stable while moving setup to `regtest`, where the follow-up generated-block path can find a valid nonce quickly.

Co-authored-by: hodlinator <hodlinator@users.noreply.github.com>

src/bench/checkblock.cpp
@@ -3,14 +3,12 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <bench/bench.h>
-#include <bench/data/block413567.raw.h>
+#include <bench/block_generator.h>
 #include <chainparams.h>
 #include <common/args.h>
 #include <consensus/validation.h>
 #include <primitives/block.h>
 #include <primitives/transaction.h>
-#include <serialize.h>
-#include <span.h>
 #include <streams.h>
 #include <util/chaintype.h>
 #include <validation.h>
@@ -18,7 +16,6 @@
 #include <cassert>
 #include <cstddef>
 #include <memory>
-#include <optional>
 #include <vector>
 
 // These are the two major time-sinks which happen after we have fully received
@@ -27,35 +24,32 @@
 
 static void DeserializeBlockTest(benchmark::Bench& bench)
 {
-    DataStream stream(benchmark::data::block413567);
+    auto stream{benchmark::GenerateBlockData()};
     std::byte a{0};
     stream.write({&a, 1}); // Prevent compaction
 
     bench.unit("block").run([&] {
         CBlock block;
         stream >> TX_WITH_WITNESS(block);
-        bool rewound = stream.Rewind(benchmark::data::block413567.size());
-        assert(rewound);
+        assert(stream.Rewind());
     });
 }
 
 static void DeserializeAndCheckBlockTest(benchmark::Bench& bench)
 {
-    DataStream stream(benchmark::data::block413567);
+    const auto& chain_params{CChainParams::RegTest(CChainParams::RegTestOptions{})};
+    auto stream{benchmark::GenerateBlockData(*chain_params)};
     std::byte a{0};
     stream.write({&a, 1}); // Prevent compaction
 
-    ArgsManager bench_args;
-    const auto chainParams = CreateChainParams(bench_args, ChainType::MAIN);
 
     bench.unit("block").run([&] {
         CBlock block; // Note that CBlock caches its checked state, so we need to recreate it here
         stream >> TX_WITH_WITNESS(block);
-        bool rewound = stream.Rewind(benchmark::data::block413567.size());
-        assert(rewound);
+        assert(stream.Rewind());
 
         BlockValidationState validationState;
-        bool checked = CheckBlock(block, validationState, chainParams->GetConsensus());
+        const bool checked{CheckBlock(block, validationState, chain_params->GetConsensus())};
         assert(checked);
     });
 }

src/bench/load_external.cpp
@@ -3,7 +3,7 @@
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
 #include <bench/bench.h>
-#include <bench/data/block413567.raw.h>
+#include <bench/block_generator.h>
 #include <chainparams.h>
 #include <flatfile.h>
 #include <node/blockstorage.h>
@@ -35,17 +35,18 @@
  */
 static void LoadExternalBlockFile(benchmark::Bench& bench)
 {
-    const auto testing_setup{MakeNoLogFileContext<const TestingSetup>(ChainType::MAIN)};
+    const auto testing_setup{MakeNoLogFileContext<const TestingSetup>(ChainType::REGTEST)};
 
     // Create a single block as in the blocks files (magic bytes, block size,
     // block data) as a stream object.
     const fs::path blkfile{testing_setup.get()->m_path_root / "blk.dat"};
     DataStream ss{};
-    auto params{testing_setup->m_node.chainman->GetParams()};
+    const auto& params{testing_setup->m_node.chainman->GetParams()};
+    const auto block_data{benchmark::GenerateBlockData(params)};
     ss << params.MessageStart();
-    ss << static_cast<uint32_t>(benchmark::data::block413567.size());
+    ss << static_cast<uint32_t>(block_data.size());
     // Use span-serialization to avoid writing the size first.
-    ss << std::span{benchmark::data::block413567};
+    ss << std::span{block_data};
 
     // Create the test file.
     {

l0rinc/bitcoin

Co-authored-by: MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>

src/bench/checkblock.cpp
@@ -3,7 +3,8 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <bench/bench.h>
-#include <bench/data/block413567.raw.h>
+#include <bench/block_generator.h>
+#include <chainparams.h>
 #include <consensus/validation.h>
 #include <kernel/chainparams.h>
 #include <primitives/block.h>
@@ -23,7 +24,7 @@
 
 static void DeserializeBlockTest(benchmark::Bench& bench)
 {
-    const auto block_data{benchmark::data::block413567};
+    const auto block_data{benchmark::GenerateBlockData()};
     bench.unit("block").run([&] {
         CBlock block;
         SpanReader{block_data} >> TX_WITH_WITNESS(block);
@@ -33,8 +34,8 @@ static void DeserializeBlockTest(benchmark::Bench& bench)
 
 static void CheckBlockTest(benchmark::Bench& bench)
 {
-    const auto& chain_params{CChainParams::Main()};
-    const auto block_data{benchmark::data::block413567};
+    const auto& chain_params{CChainParams::RegTest(CChainParams::RegTestOptions{})};
+    auto block_data{benchmark::GenerateBlockData(*chain_params)};
 
     CBlock block;
     bench.unit("block")

src/bench/load_external.cpp
@@ -3,7 +3,7 @@
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
 #include <bench/bench.h>
-#include <bench/data/block413567.raw.h>
+#include <bench/block_generator.h>
 #include <chainparams.h>
 #include <flatfile.h>
 #include <node/blockstorage.h>
@@ -34,17 +34,18 @@
  */
 static void LoadExternalBlockFile(benchmark::Bench& bench)
 {
-    const auto testing_setup{MakeNoLogFileContext<const TestingSetup>(ChainType::MAIN)};
+    const auto testing_setup{MakeNoLogFileContext<const ChainTestingSetup>(ChainType::REGTEST)};
 
     // Create a single block as in the blocks files (magic bytes, block size,
     // block data) as a stream object.
     const fs::path blkfile{testing_setup.get()->m_path_root / "blk.dat"};
     DataStream ss{};
-    auto params{testing_setup->m_node.chainman->GetParams()};
+    const auto& params{testing_setup->m_node.chainman->GetParams()};
+    const auto block_data{benchmark::GenerateBlockData(params)};
     ss << params.MessageStart();
-    ss << static_cast<uint32_t>(benchmark::data::block413567.size());
+    ss << static_cast<uint32_t>(block_data.size());
     // Use span-serialization to avoid writing the size first.
-    ss << std::span{benchmark::data::block413567};
+    ss << std::span{block_data};
 
     // Create the test file.
     {