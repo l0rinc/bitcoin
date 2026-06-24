34a032da30e04e51552d343c3d14e35c00c7ee4b
Lőrinc <pap.lorinc@gmail.com>
2026-06-23 18:30:34 -0700

    l0: use 16/32/64 LevelDB thresholds

--- src/leveldb/db/dbformat.h
+++ src/leveldb/db/dbformat.h
@@ -25,7 +25,7 @@ namespace config {
 static const int kNumLevels = 7;
 
 // Level-0 compaction is started when we hit this many files.
-static const int kL0_CompactionTrigger = 8;
+static const int kL0_CompactionTrigger = 16;
 
 // Soft limit on number of level-0 files.  We slow down writes at this point.
 static const int kL0_SlowdownWritesTrigger = 32;
