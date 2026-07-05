c0a2982cfb1fe658d220c6d569656a95bf720afb
Lőrinc <pap.lorinc@gmail.com>
2026-07-05 12:03:39 +0000

    test: accept BDB overflow parser fuzz errors

--- src/wallet/test/fuzz/wallet_bdb_parser.cpp
+++ src/wallet/test/fuzz/wallet_bdb_parser.cpp
@@ -94,6 +94,8 @@ FUZZ_TARGET(wallet_bdb_parser, .init = initialize_wallet_bdb_parser)
             error.original == "LSNs are not reset, this database is not completely flushed. Please reopen then close the database with a version that has BDB support" ||
             error.original == "Records page has odd number of records" ||
             error.original == "Bad overflow record page type" ||
+            error.original == "Overflow record has an impossible length" ||
+            error.original == "Overflow record data is larger than stated size" ||
             error.original == "BTree page has an unexpected level" ||
             error.original == "BTree Leaf page is not at level 1") {
             // Do nothing
