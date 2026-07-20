RandyMcMillan/dotfiles

net: allow up to 2 non-BIP110 outbound peers

src/net_processing.cpp
@@ -799,6 +799,9 @@ private:
     /** Number of peers with wtxid relay. */
     std::atomic<int> m_wtxid_relay_peers{0};
 
+    /** Number of outbound peers without NODE_UASF_REDUCED_DATA (BIP-110). Limited to 2. */
+    std::atomic<int> m_num_non_bip110_outbound{0};
+
     /** Number of outbound peers with m_chain_sync.m_protect. */
     int m_outbound_peers_with_protect_from_disconnect GUARDED_BY(cs_main) = 0;
 
@@ -1594,6 +1597,11 @@ void PeerManagerImpl::FinalizeNode(const CNode& node)
         assert(peer != nullptr);
         m_wtxid_relay_peers -= peer->m_wtxid_relay;
         assert(m_wtxid_relay_peers >= 0);
+        // Decrement non-BIP110 counter if this was a non-BIP110 outbound peer
+        if (node.m_is_non_bip110_outbound) {
+            --m_num_non_bip110_outbound;
+            assert(m_num_non_bip110_outbound >= 0);
+        }
     }
     CNodeState *state = State(nodeid);
     assert(state != nullptr);
@@ -1657,14 +1665,13 @@ bool PeerManagerImpl::HasAllDesirableServiceFlags(ServiceFlags services) const
 
 ServiceFlags PeerManagerImpl::GetDesirableServiceFlags(ServiceFlags services) const
 {
-    // We want to preferentially peer with other nodes that enforce UASF-ReducedData, in case of a chain split
     if (services & NODE_NETWORK_LIMITED) {
         // Limited peers are desirable when we are close to the tip.
         if (ApproximateBestBlockDepth() < NODE_NETWORK_LIMITED_ALLOW_CONN_BLOCKS) {
-            return ServiceFlags(NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_UASF_REDUCED_DATA);
+            return ServiceFlags(NODE_NETWORK_LIMITED | NODE_WITNESS);
         }
     }
-    return ServiceFlags(NODE_NETWORK | NODE_WITNESS | NODE_UASF_REDUCED_DATA);
+    return ServiceFlags(NODE_NETWORK | NODE_WITNESS);
 }
 
 PeerRef PeerManagerImpl::GetPeerRef(NodeId id) const
@@ -3536,6 +3543,19 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
         }
 
         pfrom.m_has_all_wanted_services = HasAllDesirableServiceFlags(nServices);
+        // BIP-110: Allow up to 2 non-BIP110 outbound peers.
+        if (pfrom.ExpectServicesFromConn() && !(nServices & NODE_UASF_REDUCED_DATA)) {
+            if (m_num_non_bip110_outbound >= 2) {
+                LogDebug(BCLog::NET, "peer lacks NODE_UASF_REDUCED_DATA and already have 2 non-BIP110 outbound peers, %s\n",
+                         pfrom.DisconnectMsg(fLogIPs));
+                pfrom.fDisconnect = true;
+                return;
+            }
+            ++m_num_non_bip110_outbound;
+            pfrom.m_is_non_bip110_outbound = true;
+            LogDebug(BCLog::NET, "connected to non-BIP110 outbound peer (%d/2), %s\n",
+                     m_num_non_bip110_outbound.load(), pfrom.ConnectionTypeAsString());
+        }
         peer->m_their_services = nServices;
         pfrom.SetAddrLocal(addrMe);
         peer->m_starting_height = starting_height;

test/functional/p2p_handshake.py
@@ -21,12 +21,13 @@ from test_framework.p2p import P2PInterface
 from test_framework.util import p2p_port
 
 
-# Desirable service flags for outbound non-pruned and pruned peers. Note that
-# the desirable service flags for pruned peers are dynamic and only apply if
-#  1. the peer's service flag NODE_NETWORK_LIMITED is set *and*
-#  2. the local chain is close to the tip (<24h)
-DESIRABLE_SERVICE_FLAGS_FULL = NODE_NETWORK | NODE_WITNESS | NODE_UASF_REDUCED_DATA
-DESIRABLE_SERVICE_FLAGS_PRUNED = NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_UASF_REDUCED_DATA
+# Base service flags (without BIP-110)
+BASE_SERVICE_FLAGS_FULL = NODE_NETWORK | NODE_WITNESS
+BASE_SERVICE_FLAGS_PRUNED = NODE_NETWORK_LIMITED | NODE_WITNESS
+
+# Full service flags (with BIP-110)
+FULL_SERVICE_FLAGS_FULL = NODE_NETWORK | NODE_WITNESS | NODE_UASF_REDUCED_DATA
+FULL_SERVICE_FLAGS_PRUNED = NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_UASF_REDUCED_DATA
 
 
 class P2PHandshakeTest(BitcoinTestFramework):
@@ -72,19 +73,52 @@ class P2PHandshakeTest(BitcoinTestFramework):
 
     def run_test(self):
         node = self.nodes[0]
-        self.log.info("Check that lacking desired service flags leads to disconnect (non-pruned peers)")
+
+        self.log.info("Check that peers lacking base service flags are disconnected")
+        # These should always be disconnected regardless of BIP-110 counter
         self.test_desirable_service_flags(node, [NODE_NONE, NODE_NETWORK, NODE_WITNESS],
-                                          DESIRABLE_SERVICE_FLAGS_FULL, expect_disconnect=True)
+                                          BASE_SERVICE_FLAGS_FULL, expect_disconnect=True)
+
+        self.log.info("Check that first 2 non-BIP110 peers connect, 3rd is rejected")
+        # Connect first 2 non-BIP110 peers and keep them connected
+        non_bip110_services = NODE_NETWORK | NODE_WITNESS
+        if self.options.v2transport:
+            non_bip110_services |= NODE_P2P_V2
+        peer1 = node.add_outbound_p2p_connection(
+            P2PInterface(), p2p_idx=0, wait_for_disconnect=False,
+            connection_type="outbound-full-relay", services=non_bip110_services,
+            supports_v2_p2p=self.options.v2transport, advertise_v2_p2p=self.options.v2transport)
+        peer1.sync_with_ping()
+        peer2 = node.add_outbound_p2p_connection(
+            P2PInterface(), p2p_idx=1, wait_for_disconnect=False,
+            connection_type="outbound-full-relay", services=non_bip110_services,
+            supports_v2_p2p=self.options.v2transport, advertise_v2_p2p=self.options.v2transport)
+        peer2.sync_with_ping()
+        assert len(node.getpeerinfo()) == 2
+        # Third non-BIP110 peer should be rejected
+        with node.assert_debug_log(["peer lacks NODE_UASF_REDUCED_DATA and already have 2 non-BIP110 outbound peers"]):
+            node.add_outbound_p2p_connection(
+                P2PInterface(), p2p_idx=2, wait_for_disconnect=True,
+                connection_type="outbound-full-relay", services=non_bip110_services,
+                supports_v2_p2p=self.options.v2transport, advertise_v2_p2p=self.options.v2transport)
+        # Clean up - disconnect the 2 non-BIP110 peers
+        peer1.peer_disconnect()
+        peer2.peer_disconnect()
+        peer1.wait_for_disconnect()
+        peer2.wait_for_disconnect()
+        self.wait_until(lambda: len(node.getpeerinfo()) == 0)
+
+        self.log.info("Check that BIP110 peers always connect")
         self.test_desirable_service_flags(node, [NODE_NETWORK | NODE_WITNESS | NODE_UASF_REDUCED_DATA],
-                                          DESIRABLE_SERVICE_FLAGS_FULL, expect_disconnect=False)
+                                          BASE_SERVICE_FLAGS_FULL, expect_disconnect=False)
 
         self.log.info("Check that limited peers are only desired if the local chain is close to the tip (<24h)")
         self.generate_at_mocktime(int(time.time()) - 25 * 3600)  # tip outside the 24h window, should fail
         self.test_desirable_service_flags(node, [NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_UASF_REDUCED_DATA],
-                                          DESIRABLE_SERVICE_FLAGS_FULL, expect_disconnect=True)
+                                          BASE_SERVICE_FLAGS_FULL, expect_disconnect=True)
         self.generate_at_mocktime(int(time.time()) - 23 * 3600)  # tip inside the 24h window, should succeed
         self.test_desirable_service_flags(node, [NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_UASF_REDUCED_DATA],
-                                          DESIRABLE_SERVICE_FLAGS_PRUNED, expect_disconnect=False)
+                                          BASE_SERVICE_FLAGS_PRUNED, expect_disconnect=False)
 
         self.log.info("Check that feeler connections get disconnected immediately")
         with node.assert_debug_log(["feeler connection completed"]):

l0rinc/bitcoin

Github-Pull: bitcoinknots/bitcoin#238
Rebased-From: f62f5fda667f899c5d93998858ffb46a7467931a
(cherry picked from commit d8bfc2d88807a8dd2f3492ef661dfdd129af315d)

src/net_processing.cpp
@@ -849,6 +849,9 @@ private:
     /** Number of peers with wtxid relay. */
     std::atomic<int> m_wtxid_relay_peers{0};
 
+    /** Number of outbound peers without NODE_REDUCED_DATA (BIP-110). Limited to 2. */
+    std::atomic<int> m_num_non_bip110_outbound{0};
+
     /** Number of outbound peers with m_chain_sync.m_protect. */
     int m_outbound_peers_with_protect_from_disconnect GUARDED_BY(cs_main) = 0;
 
@@ -1720,6 +1723,11 @@ void PeerManagerImpl::FinalizeNode(const CNode& node)
         assert(peer != nullptr);
         m_wtxid_relay_peers -= peer->m_wtxid_relay;
         assert(m_wtxid_relay_peers >= 0);
+        // Decrement non-BIP110 counter if this was a non-BIP110 outbound peer
+        if (node.m_is_non_bip110_outbound) {
+            --m_num_non_bip110_outbound;
+            assert(m_num_non_bip110_outbound >= 0);
+        }
     }
     CNodeState *state = State(nodeid);
     assert(state != nullptr);
@@ -1790,14 +1798,13 @@ bool PeerManagerImpl::HasAllDesirableServiceFlags(ServiceFlags services) const
 
 ServiceFlags PeerManagerImpl::GetDesirableServiceFlags(ServiceFlags services) const
 {
-    // We want to preferentially peer with other nodes that enforce UASF-ReducedData, in case of a chain split
     if (services & NODE_NETWORK_LIMITED) {
         // Limited peers are desirable when we are close to the tip.
         if (ApproximateBestBlockDepth() < NODE_NETWORK_LIMITED_ALLOW_CONN_BLOCKS) {
-            return ServiceFlags(NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_REDUCED_DATA);
+            return ServiceFlags(NODE_NETWORK_LIMITED | NODE_WITNESS);
         }
     }
-    return ServiceFlags(NODE_NETWORK | NODE_WITNESS | NODE_REDUCED_DATA);
+    return ServiceFlags(NODE_NETWORK | NODE_WITNESS);
 }
 
 PeerRef PeerManagerImpl::GetPeerRef(NodeId id) const
@@ -3832,6 +3839,41 @@ void PeerManagerImpl::ProcessMessage(Peer& peer, CNode& pfrom, const std::string
             // it to nodes with a version before 70016, as no software is known to support
             // BIP155 that doesn't announce at least that protocol version number.
             MakeAndPushMessage(pfrom, NetMsgType::SENDADDRV2);
+        }
+
+        pfrom.m_has_all_wanted_services = HasAllDesirableServiceFlags(nServices);
+        // BIP-110: Allow up to 2 non-BIP110 outbound peers.
+        if (pfrom.ExpectServicesFromConn() && !(nServices & NODE_REDUCED_DATA)) {
+            if (m_num_non_bip110_outbound >= 2) {
+                LogDebug(BCLog::NET, "peer lacks NODE_REDUCED_DATA and already have 2 non-BIP110 outbound peers, %s\n",
+                         pfrom.DisconnectMsg(fLogIPs));
+                pfrom.fDisconnect = true;
+                return;
+            }
+            ++m_num_non_bip110_outbound;
+            pfrom.m_is_non_bip110_outbound = true;
+            LogDebug(BCLog::NET, "connected to non-BIP110 outbound peer (%d/2), %s\n",
+                     m_num_non_bip110_outbound.load(), pfrom.ConnectionTypeAsString());
+        }
+        peer->m_their_services = nServices;
+        pfrom.SetAddrLocal(addrMe);
+        peer->m_starting_height = starting_height;
+
+        // Only initialize the Peer::TxRelay m_relay_txs data structure if:
+        // - this isn't an outbound block-relay-only connection, and
+        // - this isn't an outbound feeler connection, and
+        // - fRelay=true (the peer wishes to receive transaction announcements)
+        //   or we're offering NODE_BLOOM to this peer. NODE_BLOOM means that
+        //   the peer may turn on transaction relay later.
+        if (!pfrom.IsBlockOnlyConn() &&
+            !pfrom.IsFeelerConn() &&
+            (fRelay || (peer->m_our_services & NODE_BLOOM))) {
+            auto* const tx_relay = peer->SetTxRelay();
+            {
+                LOCK(tx_relay->m_bloom_filter_mutex);
+                tx_relay->m_relay_txs = fRelay; // set to true after we get the first filter* message
+            }
+            if (fRelay) pfrom.m_relays_txs = true;
         }
 
         if (greatest_common_version >= WTXID_RELAY_VERSION && m_txreconciliation) {

test/functional/p2p_handshake.py
@@ -18,6 +18,7 @@ from test_framework.messages import (
     NODE_NONE,
     NODE_P2P_V2,
     NODE_WITNESS,
+    msg_verack,
     msg_version,
 )
 from test_framework.p2p import (
@@ -36,8 +37,14 @@ from test_framework.util import (
 # the desirable service flags for pruned peers are dynamic and only apply if
 #  1. the peer's service flag NODE_NETWORK_LIMITED is set *and*
 #  2. the local chain is close to the tip (<24h)
-DESIRABLE_SERVICE_FLAGS_FULL = NODE_NETWORK | NODE_WITNESS | NODE_REDUCED_DATA
-DESIRABLE_SERVICE_FLAGS_PRUNED = NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_REDUCED_DATA
+
+# Base service flags (without BIP-110)
+BASE_SERVICE_FLAGS_FULL = NODE_NETWORK | NODE_WITNESS
+BASE_SERVICE_FLAGS_PRUNED = NODE_NETWORK_LIMITED | NODE_WITNESS
+
+# Full service flags (with BIP-110)
+FULL_SERVICE_FLAGS_FULL = NODE_NETWORK | NODE_WITNESS | NODE_REDUCED_DATA
+FULL_SERVICE_FLAGS_PRUNED = NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_REDUCED_DATA
 
 
 class P2PHandshakeTest(BitcoinTestFramework):
@@ -104,19 +111,51 @@ class P2PHandshakeTest(BitcoinTestFramework):
             verack_conn.send_and_ping(msg_verack())
         node.disconnect_p2ps()
 
-        self.log.info("Check that lacking desired service flags leads to disconnect (non-pruned peers)")
+        self.log.info("Check that peers lacking base service flags are disconnected")
+        # These should always be disconnected regardless of BIP-110 counter
         self.test_desirable_service_flags(node, [NODE_NONE, NODE_NETWORK, NODE_WITNESS],
-                                          DESIRABLE_SERVICE_FLAGS_FULL, expect_disconnect=True)
+                                          BASE_SERVICE_FLAGS_FULL, expect_disconnect=True)
+
+        self.log.info("Check that first 2 non-BIP110 peers connect, 3rd is rejected")
+        # Connect first 2 non-BIP110 peers and keep them connected
+        non_bip110_services = NODE_NETWORK | NODE_WITNESS
+        if self.options.v2transport:
+            non_bip110_services |= NODE_P2P_V2
+        peer1 = node.add_outbound_p2p_connection(
+            P2PInterface(), p2p_idx=0, wait_for_disconnect=False,
+            connection_type="outbound-full-relay", services=non_bip110_services,
+            supports_v2_p2p=self.options.v2transport, advertise_v2_p2p=self.options.v2transport)
+        peer1.sync_with_ping()
+        peer2 = node.add_outbound_p2p_connection(
+            P2PInterface(), p2p_idx=1, wait_for_disconnect=False,
+            connection_type="outbound-full-relay", services=non_bip110_services,
+            supports_v2_p2p=self.options.v2transport, advertise_v2_p2p=self.options.v2transport)
+        peer2.sync_with_ping()
+        assert len(node.getpeerinfo()) == 2
+        # Third non-BIP110 peer should be rejected
+        with node.assert_debug_log(["peer lacks NODE_REDUCED_DATA and already have 2 non-BIP110 outbound peers"]):
+            node.add_outbound_p2p_connection(
+                P2PInterface(), p2p_idx=2, wait_for_disconnect=True,
+                connection_type="outbound-full-relay", services=non_bip110_services,
+                supports_v2_p2p=self.options.v2transport, advertise_v2_p2p=self.options.v2transport)
+        # Clean up - disconnect the 2 non-BIP110 peers
+        peer1.peer_disconnect()
+        peer2.peer_disconnect()
+        peer1.wait_for_disconnect()
+        peer2.wait_for_disconnect()
+        self.wait_until(lambda: len(node.getpeerinfo()) == 0)
+
+        self.log.info("Check that BIP110 peers always connect")
         self.test_desirable_service_flags(node, [NODE_NETWORK | NODE_WITNESS | NODE_REDUCED_DATA],
-                                          DESIRABLE_SERVICE_FLAGS_FULL, expect_disconnect=False)
+                                          BASE_SERVICE_FLAGS_FULL, expect_disconnect=False)
 
         self.log.info("Check that limited peers are only desired if the local chain is close to the tip (<24h)")
         self.generate_at_mocktime(int(time.time()) - 25 * 3600)  # tip outside the 24h window, should fail
         self.test_desirable_service_flags(node, [NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_REDUCED_DATA],
-                                          DESIRABLE_SERVICE_FLAGS_FULL, expect_disconnect=True)
+                                          BASE_SERVICE_FLAGS_FULL, expect_disconnect=True)
         self.generate_at_mocktime(int(time.time()) - 23 * 3600)  # tip inside the 24h window, should succeed
         self.test_desirable_service_flags(node, [NODE_NETWORK_LIMITED | NODE_WITNESS | NODE_REDUCED_DATA],
-                                          DESIRABLE_SERVICE_FLAGS_PRUNED, expect_disconnect=False)
+                                          BASE_SERVICE_FLAGS_PRUNED, expect_disconnect=False)
 
         self.log.info("Check that feeler connections get disconnected immediately")
         with node.assert_debug_log(["feeler connection completed"]):