bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

QA: Add forbid_msgs param to TestNode.wait_for_debug_log

test/functional/test_framework/test_node.py
@@ -476,7 +476,7 @@ class TestNode():
         self._raise_assertion_error('Expected messages "{}" does not partially match log:\n\n{}\n\n'.format(str(expected_msgs), print_log))
 
     @contextlib.contextmanager
-    def wait_for_debug_log(self, expected_msgs, timeout=60):
+    def wait_for_debug_log(self, expected_msgs, timeout=60, *, forbid_msgs=()):
         """
         Block until we see a particular debug log message fragment or until we exceed the timeout.
         Return:
@@ -492,6 +492,13 @@ class TestNode():
             with open(self.debug_log_path, "rb") as dl:
                 dl.seek(prev_size)
                 log = dl.read()
+
+            for msg in forbid_msgs:
+                if msg in log:
+                    print_log = " - " + "\n - ".join(log.decode("utf8", errors="replace").splitlines())
+                    self._raise_assertion_error(
+                        'Forbidden message "{}" partially matched log:\n\n{}\n\n'.format(
+                            str(msg), print_log))
 
             for expected_msg in expected_msgs:
                 if expected_msg not in log:

l0rinc/bitcoin

(cherry picked from commit 0df056a2320d2ffa774996381dfac524298c9df4)

test/functional/test_framework/test_node.py
@@ -618,7 +618,7 @@ class TestNode():
                                     f'not found in log:\n\n{join_log(log)}\n\n')
 
     @contextlib.contextmanager
-    def busy_wait_for_debug_log(self, expected_msgs, timeout=60):
+    def busy_wait_for_debug_log(self, expected_msgs, timeout=60, *, forbid_msgs=()):
         """
         Block until we see a particular debug log message fragment or until we exceed the timeout.
         """
@@ -633,9 +633,18 @@ class TestNode():
                 dl.seek(prev_size)
                 log = dl.read()
 
-            while remaining_expected and remaining_expected[-1] in log:
-                remaining_expected.pop()
-            if not remaining_expected:
+            for msg in forbid_msgs:
+                if msg in log:
+                    print_log = " - " + "\n - ".join(log.decode("utf8", errors="replace").splitlines())
+                    self._raise_assertion_error(
+                        'Forbidden message "{}" partially matched log:\n\n{}\n\n'.format(
+                            str(msg), print_log))
+
+            for expected_msg in expected_msgs:
+                if expected_msg not in log:
+                    found = False
+
+            if found:
                 return
 
             if time.time() >= time_end: