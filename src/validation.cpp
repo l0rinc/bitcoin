bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Roll back (tip - 1) prune locks when doing a reorg

src/validation.cpp
@@ -2553,13 +2553,13 @@ bool CChainState::DisconnectTip(BlockValidationState& state, DisconnectedBlockTr
     LogPrint(BCLog::BENCH, "- Disconnect block: %.2fms\n", (GetTimeMicros() - nStart) * MILLI);
 
     {
-        // Prune locks that began at or after the tip should be moved backward so they get a chance to reorg
+        // Prune locks that began around the tip should be moved backward so they get a chance to reorg
         const int max_height_first{pindexDelete->nHeight - 1};
         for (auto& prune_lock : m_blockman.m_prune_locks) {
-            if (prune_lock.second.height_first <= max_height_first) continue;
+            if (prune_lock.second.height_first < max_height_first) continue;
 
-            prune_lock.second.height_first = max_height_first;
-            LogPrint(BCLog::PRUNE, "%s prune lock moved back to %d\n", prune_lock.first, max_height_first);
+            --prune_lock.second.height_first;
+            LogPrint(BCLog::PRUNE, "%s prune lock moved back to %d\n", prune_lock.first, prune_lock.second.height_first);
         }
     }
 

l0rinc/bitcoin

(cherry picked from commit 8ee12141570140f56f5cfefd793825a8787656a6)
https://github.com/bitcoinknots/bitcoin/commit/8ee12141570140f56f5cfefd793825a8787656a6

src/validation.cpp
@@ -2976,13 +2976,13 @@ bool Chainstate::DisconnectTip(BlockValidationState& state, DisconnectedBlockTra
              Ticks<MillisecondsDouble>(SteadyClock::now() - time_start));
 
     {
-        // Prune locks that began at or after the tip should be moved backward so they get a chance to reorg
+        // Prune locks that began around the tip should be moved backward so they get a chance to reorg
         const int max_height_first{pindexDelete->nHeight - 1};
         for (auto& prune_lock : m_blockman.m_prune_locks) {
-            if (prune_lock.second.height_first <= max_height_first) continue;
+            if (prune_lock.second.height_first < max_height_first) continue;
 
-            prune_lock.second.height_first = max_height_first;
-            LogDebug(BCLog::PRUNE, "%s prune lock moved back to %d\n", prune_lock.first, max_height_first);
+            --prune_lock.second.height_first;
+            LogDebug(BCLog::PRUNE, "%s prune lock moved back to %d\n", prune_lock.first, prune_lock.second.height_first);
         }
     }
 