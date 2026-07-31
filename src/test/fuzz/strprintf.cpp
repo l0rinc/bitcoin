deab6f244be28c94db62864d82b25e67c4aaaa86
Lőrinc <pap.lorinc@gmail.com>
2026-07-31 01:18:46 +0000

    fuzz: narrow strprintf format exclusions

--- src/test/fuzz/strprintf.cpp
+++ src/test/fuzz/strprintf.cpp
@@ -33,6 +33,92 @@ std::string EscapePercentSigns(std::string_view input)
     return escaped;
 }
 
+struct FormatStringProperties {
+    bool has_large_width_or_precision{false};
+    bool has_positional_variable_width_or_precision{false};
+    bool has_variable_width_or_precision{false};
+    bool has_char_conversion{false};
+};
+
+FormatStringProperties InspectFormatString(std::string_view format_string)
+{
+    FormatStringProperties result;
+
+    for (size_t pos{0}; pos < format_string.size();) {
+        if (format_string[pos] != '%') {
+            ++pos;
+            continue;
+        }
+        if (++pos == format_string.size()) break;
+        if (format_string[pos] == '%') {
+            ++pos;
+            continue;
+        }
+
+        bool positional_mode{false};
+        bool initial_width_set{false};
+        bool initial_digits_nonzero{false};
+        const auto consume_digits = [&] {
+            const size_t start{pos};
+            while (pos < format_string.size() && IsDigit(format_string[pos])) {
+                initial_digits_nonzero |= format_string[pos] != '0';
+                ++pos;
+            }
+            return pos - start;
+        };
+
+        const size_t initial_digits{consume_digits()};
+        if (initial_digits >= 7) result.has_large_width_or_precision = true;
+        if (initial_digits != 0 && pos < format_string.size() && format_string[pos] == '$') {
+            positional_mode = true;
+            ++pos;
+        } else {
+            initial_width_set = initial_digits != 0 && initial_digits_nonzero;
+        }
+
+        const auto consume_width_or_precision = [&] {
+            if (pos < format_string.size() && IsDigit(format_string[pos])) {
+                const size_t digits{consume_digits()};
+                if (digits >= 7) result.has_large_width_or_precision = true;
+            } else if (pos < format_string.size() && format_string[pos] == '*') {
+                result.has_variable_width_or_precision = true;
+                ++pos;
+                const size_t positional_digits{consume_digits()};
+                if (positional_mode) {
+                    result.has_positional_variable_width_or_precision = true;
+                    if (positional_digits != 0 && pos < format_string.size() && format_string[pos] == '$') ++pos;
+                }
+            }
+        };
+
+        if (!initial_width_set) {
+            while (pos < format_string.size() &&
+                   (format_string[pos] == '#' || format_string[pos] == '0' ||
+                    format_string[pos] == '-' || format_string[pos] == ' ' ||
+                    format_string[pos] == '+')) {
+                ++pos;
+            }
+            consume_width_or_precision();
+        }
+        if (pos < format_string.size() && format_string[pos] == '.') {
+            ++pos;
+            consume_width_or_precision();
+        }
+        while (pos < format_string.size() &&
+               (format_string[pos] == 'l' || format_string[pos] == 'h' ||
+                format_string[pos] == 'L' || format_string[pos] == 'j' ||
+                format_string[pos] == 'z' || format_string[pos] == 't')) {
+            ++pos;
+        }
+        if (pos < format_string.size()) {
+            result.has_char_conversion |= format_string[pos] == 'c';
+            ++pos;
+        }
+    }
+
+    return result;
+}
+
 FUZZ_TARGET(str_printf)
 {
     FuzzedDataProvider fuzzed_data_provider(buffer.data(), buffer.size());
@@ -42,7 +128,7 @@ FUZZ_TARGET(str_printf)
         assert(tfm::format(tfm::RuntimeFormat{EscapePercentSigns(format_string)}) == format_string);
     }
 
-    const int digits_in_format_specifier = std::count_if(format_string.begin(), format_string.end(), IsDigit);
+    const FormatStringProperties format_properties{InspectFormatString(format_string)};
 
     // Avoid triggering the following crash bug:
     // * strprintf("%987654321000000:", 1);
@@ -50,16 +136,11 @@ FUZZ_TARGET(str_printf)
     // Avoid triggering the following OOM bug:
     // * strprintf("%.222222200000000$", 1.1);
     //
-    // Upstream bug report: https://github.com/c42f/tinyformat/issues/70
-    if (format_string.find('%') != std::string::npos && digits_in_format_specifier >= 7) {
-        return;
-    }
-
-    // Avoid triggering the following crash bug:
+    // Also avoid the following crash bug:
     // * strprintf("%1$*1$*", -11111111);
     //
     // Upstream bug report: https://github.com/c42f/tinyformat/issues/70
-    if (format_string.find('%') != std::string::npos && format_string.find('$') != std::string::npos && format_string.find('*') != std::string::npos && digits_in_format_specifier > 0) {
+    if (format_properties.has_large_width_or_precision || format_properties.has_positional_variable_width_or_precision) {
         return;
     }
 
@@ -94,14 +175,14 @@ FUZZ_TARGET(str_printf)
     } catch (const tinyformat::format_error&) {
     }
 
-    if (format_string.find('%') != std::string::npos && format_string.find('c') != std::string::npos) {
+    if (format_properties.has_char_conversion) {
         // Avoid triggering the following:
         // * strprintf("%c", 1.31783e+38);
         // tinyformat.h:244:36: runtime error: 1.31783e+38 is outside the range of representable values of type 'char'
         return;
     }
 
-    if (format_string.find('%') != std::string::npos && format_string.find('*') != std::string::npos) {
+    if (format_properties.has_variable_width_or_precision) {
         // Avoid triggering the following:
         // * strprintf("%*", -2.33527e+38);
         // tinyformat.h:283:65: runtime error: -2.33527e+38 is outside the range of representable values of type 'int'
